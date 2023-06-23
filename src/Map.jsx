bef7e098cd2bfa83c01bdfba666770468e9a0e0b
Jeff Freedman <freedman.jeffrey@gmail.com>
2023-06-23 21:15:52 +0000

    Update 4 files

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import "./Map.css";
 import Legend from "./Legend";
 
-const INITIAL_MAP_CONFIG = { center: [41.98311, 2.82493], zoom: 14 };
+const polyline = [
+  [51.515, -0.09],
+  [51.52, -0.1],
+  [51.52, -100],
+]
+
+const limeOptions = { color: 'lime' }
+
+const INITIAL_MAP_CONFIG = { 
+  center: [55, 0], 
+  zoom: 6 
+  };
 
 function Map() {
   const [map, setMap] = useState(null);
@@ -21,6 +32,9 @@ function Map() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      
+      <Polyline pathOptions={limeOptions} positions={polyline} />
+
       <Legend map={map} />
     </MapContainer>
   );
